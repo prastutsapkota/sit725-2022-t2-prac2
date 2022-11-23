83f7d7732a0cfa8db610ed720d35e55f311f7831
prastutsapkota <prastut.sapkota@gmail.com>
2022-11-23 18:35:56 +1100

    added post

--- server.js
+++ server.js
@@ -11,13 +11,26 @@ const addNumbers = (number1, number2) => {
         var result = num1 + num2;
         return result;
 }
+
+const mulNumbers = (number1, number2) => {
+    var num1 = parseInt(number1)
+    var num2 = parseInt(number2)
+    var result = num1 * num2;
+    return result;
+}
+
 app.get("/addTwoNumbers", (req,res) => {
         var number1 = req.query.number1;
         var number2 = req.query.number2;
         var result = addNumbers(number1, number2)
         res.json({statusCode: 200, data: result, message:'Success'})
     })
-
+app.post('/mulTwoNumbers', (req, res) => {
+    var number1 = req.query.number1;
+    var number2 = req.query.number2;
+    var result = mulNumbers(number1, number2)
+    res.json({statusCode: 200, data: result, message:'Success'})
+})
 var port = process.env.port || 3000;
 
 app.listen(port, () =>{
